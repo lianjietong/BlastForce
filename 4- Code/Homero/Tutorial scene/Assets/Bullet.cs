df56b6bf6a6637761e65565104afde674af31eb0
Homer Garza <hgarza2017@my.fit.edu>
2020-03-13 16:02:23 -0400

    Updated

---
+++ 4- Code/Homero/Tutorial scene/Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody rb;
+    public float accelerationPower = 1200f;
+    //public Camera cam;
+    //Vector3 mousePos;
+
+
+    
+    void Update(){
+        float thrust = accelerationPower * Time.deltaTime;
+        thrust = Mathf.Abs(thrust);
+        rb.AddForce(transform.right * thrust);
+    }
+
+    void OnTriggerEnter(Collider hitInfo){
+        //Debug.Log(hitInfo.name);
+
+        if(hitInfo.name != "Player"){   //if bullet hits anything other than player it will be destroyed
+            Destroy(gameObject);
+        }
+    }
+
+
+}
