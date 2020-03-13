4c13e16ad7a57d71f18545c61f2b523ee75f7c23
Homer Garza <hgarza2017@my.fit.edu>
2020-03-13 12:57:07 -0400

    updated

--- 4- Code/Homero/PlayerMovement.cs
+++ 4- Code/Homero/PlayerMovement.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Movement - x-axis for side movement, z-axis for forwards and backwards
+*/
+
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
@@ -12,57 +16,59 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -9.81f;
     Vector3 velocity;
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.5f;  // sphere around groundcheck 
     public LayerMask groundMask;
     public float jumpHeight = 3f;
-    public float rotationSpeed = 600;
+    public float rotationSpeed = 1;
     bool isGrounded;
+
+    public Camera cam;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        Move();
+    }
+
+    public void Move(){
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
-        
+
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        //Vector3 move = transform.right * x + transform.forward * z;
-        //transform.localRotation = Quaternion.Euler(x, 0f , 0f);
-        Vector3 rotation = new Vector3(0f, x * rotationSpeed * Time.deltaTime, 0f);
-        //Vector3 move = new Vector3(x, 0f, z);   //contantly moves and rotates character
-        Vector3 move = new Vector3(0f, 0f, z);
-        move = transform.TransformDirection(move);
-        transform.Rotate(rotation);
-
+        Vector3 rotation = new Vector3(0f, z * rotationSpeed * Time.deltaTime, 0f);
+        Vector3 move = new Vector3(x, 0f, z);                               //global coords movement
+        transform.Rotate(rotation*2);
+        
         controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded){
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-            animator.SetBool("isJumping", true);
-        }else if(isGrounded){
-            animator.SetBool("isJumping", false);
-        }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-
+        
         animator.SetFloat("Speed", (x + z));
 
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("isJumping", true);
+        }else{
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
 
-        //playerBody.Rotate(Vector3.up * move);
+        // if jumping and shooting down rocket jump
+            // jump double the height
+
+        if(Input.GetButtonDown("Jump") && Input.GetButtonDown("Fire1")){
+            // double jump?
+            // get angle of shot?
+            
+        }
+        velocity.y += (gravity * 7) * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
     }
-
-/*
-    public void onLanding(){
-        animator.SetBool("isJumping", false);
-    }*/
 }
 
-
-/*
-    might be usefule to rotate
-    transform.Rotate(0f, 180f, 0f)
-*/
--- 4- Code/Homero/Weapon.cs
+++ 4- Code/Homero/Weapon.cs
@@ -7,19 +7,64 @@ public class Weapon : MonoBehaviour
 
     public Transform firePoint;
     public GameObject rocketPrefab;
+    public Camera cam;
+    public Transform testObject;
+    Vector3 mousePos;
+ 
+
     // Update is called once per frame
+
+ 
+    
     void Update()
     {
+
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+        // need to grab distance to player from mouse and incoperate i think
+        float xdist = Input.mousePosition.x - firePoint.position.x;
+        float ydist = Input.mousePosition.y - firePoint.position.y;
+        Vector3 dist = new Vector3(xdist, ydist, 0f);
+
+        //mousePos = getMouseWorldPosition();
+        Vector3 aimDir = (mousePos - firePoint.position).normalized;
+        float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
+        firePoint.eulerAngles = new Vector3(0f, 0f, angle);
+        Debug.Log(angle);
         if (Input.GetButtonDown("Fire1")){
-            Shoot();
+            Shoot(angle);
         }
+
     }
 
-    void Shoot(){
+  
+
+    void Shoot(float angle){
         // shooting logic
-        Instantiate(rocketPrefab, firePoint.position, new Quaternion());
+       // firePoint.Rotate(angle, 0f, 0f);
+        Instantiate(rocketPrefab, firePoint.position, firePoint.rotation);
             
         
+    }
+
+    public Vector3 getMouseWorldPosition(){
+
+        Vector3 vec = getMouseWorldPositionz(Input.mousePosition, cam);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public  Vector3 getMouseWorldPositionz(){
+        return getMouseWorldPositionz(Input.mousePosition, cam);
+    }
+
+    public Vector3 getMouseWorldPositionz(Camera worldCamera){
+        return getMouseWorldPositionz(Input.mousePosition, worldCamera);
+    }
+
+    public Vector3 getMouseWorldPositionz(Vector3 screenPos, Camera worldCamera){
+        Vector3 worldPos = worldCamera.ScreenToWorldPoint(screenPos);
+        return worldPos;
     }
 
 
